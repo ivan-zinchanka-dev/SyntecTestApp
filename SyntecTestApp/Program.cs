a0cd2339c00bd508b5081314ebc73ae7ac44602c
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-04-20 20:30:06 +0300

    feat: net48, exception handle

--- SyntecTestApp/Program.cs
+++ SyntecTestApp/Program.cs
@@ -1,10 +1,46 @@
-﻿public static class Program
+﻿using System;
+using Syntec.MMICommon;
+using Syntec.OpenCNC;
+using Syntec.Remote;
+using SyntecRemote;
+
+public static class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Привет мир!");
-        Console.WriteLine("Hello, World!");
+        AppDomain.CurrentDomain.UnhandledException += OnUnhandledExceptionCaught;
+        
+        try
+        {
+            /*Init.MMI_Phase1();
+            Console.WriteLine("MMI Phase 1");*/
+
+            /*var synRemObj = new SyntecRemoteObj("192.168.0.100", 1000);
+            Console.WriteLine("Is connected: " + synRemObj.IsConnected());*/
+        
+            var syntecRemote = new SyntecRemoteCNC("192.168.0.100");
+            bool isUsb = syntecRemote.isUSBExist();
+            Console.WriteLine("Is USB: " + isUsb);
+
+            /*var plc = Cnc.PLC;
+            Console.WriteLine("PLC: " + plc);*/
+        }
+        catch (DllNotFoundException importLibException)
+        {
+            Console.WriteLine(importLibException);
+        }
+        catch (Exception commonException)
+        {
+            Console.WriteLine(commonException);
+        }
+        
     }
 
+    private static void OnUnhandledExceptionCaught(object sender, UnhandledExceptionEventArgs eventArgs)
+    {
+        if (eventArgs.ExceptionObject is Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
 }
-
