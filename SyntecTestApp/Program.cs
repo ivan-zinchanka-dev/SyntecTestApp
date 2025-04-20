6d2b5b8dd74e50508c2b618bd208d738f5be2916
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-04-20 17:21:58 +0300

    initial

---
+++ SyntecTestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Привет мир!");
+        Console.WriteLine("Hello, World!");
+    }
+
+}
+
